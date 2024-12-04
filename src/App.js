eb7ed98a35c2a581667e00fc5a0d79c2874581e2
cmhinst2 <cmhinst2@gmail.com>
2024-12-04 10:43:22 +0900

    jsx 예제까지

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Exam1 from "./components/Exam1";
 import Exam2 from "./components/Exam2";
+import Exam3 from "./components/Exam3";
+import Exam4 from "./components/Exam4";
+import Exam5 from "./components/Exam5";
 
 function App() {
-
   // js 주석
 
   return (
@@ -10,9 +12,12 @@ function App() {
     <div>
       {/* jsx 주석 */}
       {/* ctrl + / */}
-      <h1>Hello World!</h1>
+      {/* <h1>Hello World!</h1> */}
       {/* <Exam1 /> */}
-      <Exam2 />
+      {/* <Exam2 /> */}
+      {/* <Exam3 /> */}
+      {/* <Exam4 /> */}
+      <Exam5 />
     </div>
   );
 }
